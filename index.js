36d09dc2207c60ea93829cdf9127db184899b0a3
ShamimHosen75 <developer.shamimhosen@gmail.com>
2022-05-09 02:20:00 +0600

    added delivery product coded

--- index.js
+++ index.js
@@ -46,7 +46,20 @@ async function run() {
      });
     
     
-    
+    //  delivered Product
+     app.put('/deliveredProduct/:id', async (req, res) => {
+      const id = req.params.id;
+      const product = req.body;
+      const filter = { _id: ObjectId(id)};
+      productsCollection.updateOne(filter, {
+          $set: {
+            quantity: product.quantity,
+          },
+        })
+        .then(result => {
+          res.send(result);
+        });
+    });
 
   
   } finally {
